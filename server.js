e801ee8a2c43fe817eb6d0cfcdd2b7448cf8ae79
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-01-26 17:22:03 -0500

    testing

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const PORT = process.env.PORT;
+console.log(PORT);
 // listening
 app.listen(PORT, () => {
     console.log(`I am running on port ${PORT}`);
 })
+// hello
